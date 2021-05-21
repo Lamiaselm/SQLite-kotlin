a0f67cfc1c53cb02d981a09486f6ed49eedcd74b
Selmane Lamia <hl_selmane@esi.dz>
2021-05-21 11:13:05 +0100

    roomdatabse done

--- app/src/main/java/com/example/rdvmedical/AppDatabase.kt
+++ app/src/main/java/com/example/rdvmedical/AppDatabase.kt
@@ -2,13 +2,18 @@ package com.example.rdvmedical
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.rdvmedical.Entities.Booking
+import com.example.rdvmedical.Entities.Doctor
+import com.example.rdvmedical.Entities.Treatment
 import com.example.rdvmedical.Interfaces.BookingDao
 import com.example.rdvmedical.Interfaces.DoctorDao
 import com.example.rdvmedical.Interfaces.TreatmentDao
 
-@Database
+@Database(entities = arrayOf(Doctor::class,Booking::class,Treatment::class),version = 1)
 abstract class AppDatabase :RoomDatabase () {
     abstract fun getDoctorDao():DoctorDao
     abstract fun getBookingDao():BookingDao
     abstract fun getTreatmentDao():TreatmentDao
 }
+
+
