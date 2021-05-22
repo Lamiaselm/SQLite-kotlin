44cecd182e81f65fd3a28955e03e50bc27a045f4
Selmane Lamia <hl_selmane@esi.dz>
2021-05-22 17:43:56 +0100

    scnd test done

--- app/src/main/java/com/example/rdvmedical/Interfaces/TreatmentDao.kt
+++ app/src/main/java/com/example/rdvmedical/Interfaces/TreatmentDao.kt
@@ -24,7 +24,7 @@ interface TreatmentDao {
     @Query("Select * from treatments where :currentDate<treatmentEndDate")
     fun getCurrentTreatment(currentDate: Date):List<Treatment>
     //Récuperer le traitement en cours d'un docteur donné
-    @Query("    select * from treatments where treatmentID=( select treatmentID from bookings bk join doctors dc on bk.doctorID=dc.doctorID where dc.firstName=:firstName)")
+    @Query("    select * from treatments where treatmentID=( select treatmentID from bookings bk join doctors dc on bk.doctorID=dc.doctorID where dc.firstName=:firstName and :currentDate<treatmentEndDate )")
     fun getCurrentTreatmentByDoctor(firstName:String,currentDate: Date):Treatment
 
 }
